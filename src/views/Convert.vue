fd5a1db99b63462a05091e2ac9b573ac40d88e2b
yunser <>
2019-11-10 17:31:29 +0800

    新增 坐标转换支持批量

--- src/views/Convert.vue
+++ src/views/Convert.vue
@@ -1,17 +1,18 @@
 <template>
     <my-page title="坐标转换" :page="page">
         <div class="common-container container">
-            <ui-text-field v-model="location" label="坐标" hintText="xxx, xxx" />
+            <!-- <ui-text-field v-model="location" label="坐标" hintText="xxx, xxx" /> -->
+            <textarea class="form-control" v-model="locations" rows="6" placeholder="一行一个坐标，经度在前，纬度在后"></textarea>
             <br>
             <ui-select-field class="select" v-model="from">
-                <ui-menu-item value="wgs84" title="wgs84 坐标"/>
-                <ui-menu-item value="gcj02" title="国测局坐标"/>
+                <ui-menu-item value="wgs84" title="WGS84 坐标"/>
+                <ui-menu-item value="gcj02" title="GCJ-02 坐标"/>
                 <ui-menu-item value="bd09" title="百度坐标"/>
             </ui-select-field>
             <span class="text">转</span>
             <ui-select-field class="select" v-model="to">
-                <ui-menu-item value="wgs84" title="wgs84 坐标"/>
-                <ui-menu-item value="gcj02" title="国测局坐标"/>
+                <ui-menu-item value="wgs84" title="WGS84 坐标"/>
+                <ui-menu-item value="gcj02" title="GCJ-02 坐标"/>
                 <ui-menu-item value="bd09" title="百度坐标"/>
             </ui-select-field>
             <br>
@@ -22,19 +23,22 @@
             <div class="btns">
                 <ui-raised-button primary label="转换" @click="convert" />
             </div>
-            <div class="result" v-if="result">{{ result[0] }}, {{ result[1] }}</div>
+            <div class="result" v-if="result" v-html="result"></div>
         </div>
     </my-page>
 </template>
 
 <script>
+    /* eslint-disable */
     let coordtransform = require('coordtransform')
 
     export default {
         data () {
             return {
-                location: '',
-                from: 'bd09',
+                locations: `经度113.5 纬度24.5
+经度114.5 纬度25.5`,
+                location: '113.5, 24',
+                from: 'gcj02',
                 to: 'wgs84',
                 fixedNumber: 3,
                 result: null,
@@ -58,50 +62,89 @@
         },
         methods: {
             convert() {
-                if (!this.location) {
+                if (!this.locations) {
                     this.$message({
                         type: 'danger',
                         text: '请输入坐标'
                     })
                     return
                 }
-                let match = this.location.match(/([\d.]+)[,，]\s+([\d.]+)/)
-                if (!match) {
-                    this.$message({
-                        type: 'danger',
-                        text: '坐标输入不正确'
-                    })
-                    return
+                let results = []
+                let rows = this.locations.split('\n').filter(item => item)
+                console.log('rows', rows)
+                for (let row of rows) {
+                    
+                    let match = row.match(/[\d.]+/g)
+                    console.log('match', match)
+                    // let lng = match[0]
+                    // let lat = match[1]
+                    let lng = parseFloat(match[0])
+                    let lat = parseFloat(match[1])
+                    let oneResult = ''
+                    // if (this.from === 'bd09' && this.to === 'gcj02') {
+                    //     console.log('转换')
+                    //     this.result = coordtransform[this.from + 'to' + this.to](lng, lat)
+                    // }
+                    // if (this.from === 'gcj02' && this.to === 'bd09') {
+                    //     console.log('转换')
+                    //     this.result = coordtransform[this.from + 'to' + this.to](lng, lat)
+                    // }
+                    if (this.from === 'bd09' && this.to === 'wgs84') {
+                        console.log('转换')
+                        let result = coordtransform.bd09togcj02(lng, lat)
+                        oneResult = coordtransform.gcj02towgs84(result[0], result[1])
+                    } else if (this.from === 'wgs84' && this.to === 'bd09') {
+                        console.log('转换')
+                        let result = coordtransform.wgs84togcj02(lng, lat)
+                        oneResult = coordtransform.gcj02tobd09(result[0], result[1])
+                    } else {
+                        oneResult = coordtransform[this.from + 'to' + this.to](lng, lat)
+                    }
+                    // oneResult = [, ]
+                    oneResult = row.replace(match[0], oneResult[0].toFixed(this.fixedNumber))
+                        .replace(match[1], oneResult[1].toFixed(this.fixedNumber))
+                    console.log('oneResult', oneResult)
+                    results.push(oneResult)
                 }
-                console.log(match, match[1], match[2])
-                let lng = parseFloat(match[1])
-                let lat = parseFloat(match[2])
-                // if (this.from === 'bd09' && this.to === 'gcj02') {
-                //     console.log('转换')
-                //     this.result = coordtransform[this.from + 'to' + this.to](lng, lat)
+
+                this.result = results.join('<br>')
+                console.log('result', this.result)
+                return
+
+                // let match = this.location.match(/([\d.]+)[,，]\s+([\d.]+)/)
+                // if (!match) {
+                //     this.$message({
+                //         type: 'danger',
+                //         text: '坐标输入不正确'
+                //     })
+                //     return
                 // }
-                // if (this.from === 'gcj02' && this.to === 'bd09') {
-                //     console.log('转换')
-                //     this.result = coordtransform[this.from + 'to' + this.to](lng, lat)
-                // }
-                if (this.from === 'bd09' && this.to === 'wgs84') {
-                    console.log('转换')
-                    let result = coordtransform.bd09togcj02(lng, lat)
-                    this.result = coordtransform.gcj02towgs84(result[0], result[1])
-                } else if (this.from === 'wgs84' && this.to === 'bd09') {
-                    console.log('转换')
-                    let result = coordtransform.wgs84togcj02(lng, lat)
-                    this.result = coordtransform.gcj02tobd09(result[0], result[1])
-                } else {
-                    this.result = coordtransform[this.from + 'to' + this.to](lng, lat)
-                }
-                this.result = [this.result[0].toFixed(this.fixedNumber), this.result[1].toFixed(this.fixedNumber)]
+                // console.log(match, match[0], match[1])
+                
             }
         }
     }
 </script>
 
 <style lang="scss" scoped>
+textarea.form-control {
+    height: auto;
+}
+.form-control {
+    display: block;
+    width: 100%;
+    max-width: 400px;
+    height: 33px;
+    padding: 6px 12px;
+    font-size: 14px;
+    line-height: 1.42;
+    color: #55595c;
+    background-color: #fff;
+    background-image: none;
+    border: 1px solid #ccc;
+    border-radius: 2px;
+    outline: none;
+}
 .container {
     max-width: 400px;
 }
